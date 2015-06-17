36034a2cf75e3add01f9ba2c39eaa5b458cc37a0
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-17 10:48:57 -0700

    Add N-Triples Serialization

--- spec/controllers/dataset_controller_spec.rb
+++ spec/controllers/dataset_controller_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe DatasetController do
         expect(response.content_type).to eq "application/ld+json"
       end
     end
+    context "n-triples" do
+      before do
+        get :index, :format => :nt
+      end
+      it "should return a graph" do
+        expect(RDF::NTriples::Reader.new(response.body).statements.to_a.length).not_to eq 0
+      end
+      it "should be n-triples" do
+        expect(response.content_type).to eq "application/n-triples"
+      end
+    end
   end
 end
