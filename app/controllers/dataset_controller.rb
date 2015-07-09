0b2609fd7f53890afaad0e8bf9246bd8bf5e71ec
Steven Anderson <sanderson@bpl.org>
2015-07-09 11:38:06 -0400

    Add support to detect invalid serializations.
    This adds unit tests to catch a case of an invalid serialization.
    It additionally outputs a better error message if someone uses
    this with, say, a default "text/html" response and doesn't understand
    why they then get a 500 error.

--- app/controllers/dataset_controller.rb
+++ app/controllers/dataset_controller.rb
@@ -1,6 +1,11 @@
 class DatasetController < ApplicationController
   def index
     @data = built_dataset
+
+    if !renderer_mapping.keys.include?(request.format.symbol)
+      raise ActionController::RoutingError.new("Invalid response format specified. Valid response formats are: #{renderer_mapping.keys.join(', ')} (#{renderer_mapping_to_strings.join(', ')}). Example url of how to set format: #{request.base_url}?format=jsonld")
+    end
+
     respond_to do |f|
       renderer_mapping.each do |format, renderer|
         f.send(format) do
@@ -22,6 +27,14 @@ class DatasetController < ApplicationController
       :jsonld => lambda { |data| data.dump(:jsonld, :standard_prefixes => true) },
       :ttl => lambda { |data| data.dump(:ttl) }
     }
+  end
+
+  def renderer_mapping_to_strings
+    result = []
+    renderer_mapping.each do |format, renderer|
+      result.append(Mime::Type.lookup_by_extension(format).to_s)
+    end
+    return result
   end
 
 end
