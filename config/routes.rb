a5f8e2f7b3d462a83642c845c4cd60b147b1c3b8
Trey Terrell <tterrell@princeton.edu>
2015-09-20 10:42:02 -0700

    Fix things like ".com" being recognized as format
    Closes #20

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "dataset#index"
   
-  get Setting.uri_endpoint_route, to: 'subject#subject'
+  get Setting.uri_endpoint_route, to: 'subject#subject', constraints: {
+    :format => /(#{ApplicationController.renderer_mapping.keys.join("|")})/
+  }
 end
--- spec/routing/subject_routing_spec.rb
+++ spec/routing/subject_routing_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe "subject routes" do
   it "should navigate to the subject controller on a single subject param" do
     expect(get "/http://dbpedia.org/resource/Berlin").to route_to :controller => "subject", :action => "subject", :subject=>"http://dbpedia.org/resource/Berlin"
   end
+  it "should navigate to the subject controller even if it's a root URI" do
+    expect(get "/http://dbpedia.org").to route_to :controller => "subject", :action => "subject", :subject => "http://dbpedia.org"
+  end
+  it "should navigate to the subject controller if a good format is provided" do
+    expect(get "/http://dbpedia.org.ttl").to route_to :controller => "subject", :action => "subject", :subject => "http://dbpedia.org", :format => "ttl"
+  end
 end
