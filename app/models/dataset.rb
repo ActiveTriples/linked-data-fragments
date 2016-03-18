9929bed944324f3dbe306652fe259de49c02b054
Justin Coyne <jcoyne@justincoyne.com>
2016-03-18 07:34:48 -0500

    Use vocabularies from rdf-vocab

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -2,7 +2,7 @@ class Dataset
   include ActiveTriples::RDFSource
   configure :type => [
     RDF::URI("http://www.w3.org/ns/hydra/core#Collection"),
-    RDF::VOID.Dataset
+    RDF::Vocab::VOID.Dataset
   ]
   apply_schema DatasetSchema
 end
