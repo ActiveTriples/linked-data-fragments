f9ba9bb1c44da0431aa143e7124654f45ca48198
Tom Johnson <tom@dp.la>
2017-01-05 12:10:04 -0800

    Rework, expand and document `BackendBase`
    This substantially refactors the base backend interface, adds a
    number of utility methods, and adds YARD annotations
    throughout. Notably:
    
      - `#add` is implemented for cases when a resource should be added, but
         not retrieved.
      - `#delete_all` is deprecated in favor of `#delete_all!`
      - `#empty?` is provided.
      - `#has_resource?` is added to check if a resource is in the backend.
    
    The `#add` and `#has_resource` methods are generally already implemented
    as part of `#retrieve`, so this may be a first step toward a generic
    implementation of that key method.
    
    Shared examples are added, and tested against a completed `Repository`
    implementation. Further work is needed to bring `Marmotta` and
    `Blazegraph` backends up to date.
    
    Some todo items (questions) are added for `#get_resource_uri`.

--- lib/linked_data_fragments/backend_base.rb
+++ lib/linked_data_fragments/backend_base.rb
@@ -1,6 +1,112 @@
 module LinkedDataFragments
+  ##
+  # A base class for RDF backends.
+  #
+  # Several abstract methods are defined, which implementations must provide 
+  # concrete versions of.
+  #
+  # @note this interface provides no method for clearing (deleting) individual
+  #   resources from the cache.
   class BackendBase
+    ##
+    # @!attribute [rw] cache_backend_url
+    #   @return [String, nil] a target url in string form; `nil` may be 
+    #     returned when the backend repository is not remote.
+    # @!attribute [rw] cache_backend_context
+    #   @return [String, nil] the context URI for the cache. When the backend 
+    #     supports quads, statements will be stored in this context. `nil` 
+    #     selects the default context.
+    #   @see https://www.w3.org/TR/rdf11-concepts/#section-dataset for 
+    #     information about named graphs ("contexts") in the RDF abstract syntax
+    #   @see RDF::Dataset for documentation covering named graph support in 
+    #     RDF.rb
+    #   @see RDF::Dataset#supports? for details about `:graph_name` support in 
+    #     Repositories
     attr_accessor :cache_backend_url, :cache_backend_context
+    
+    ##
+    # @abstract Add a resource to the backend. This method retrieves the 
+    #   resource from its URI.
+    # 
+    # @return [void]
+    # @raise [IOError, Net::HTTPError] when retrieving the resource fails with
+    #   a non-2xx HTTP status code.
+    # @see RDF::Graph#load  this is typical implementation
+    def add(uri)
+      raise NotImplementedError, 
+            "#{self.class} should implement `#empty?`, but does not."
+    end
 
+    ##
+    # @deprecated Use {#delete_all!} instead.
+    # @return [void]
+    def delete_all
+      warn "[DEPRECATION] `#{self.class}#delete_all` is deprecated; " \
+           "use `#{self.class}#delete_all! instead. Called from: " \
+           "#{Gem.location_of_caller.join(':')}"
+      delete_all!
+    end
+
+    ##
+    # @abstract Implementations must remove all resources from the backend.
+    # 
+    # @return [void]
+    def delete_all!
+      raise NotImplementedError, 
+            "#{self.class} should implement `#delete_all!`, but does not."
+    end
+
+    ##
+    # @abstract
+    # @return [Boolean] `true` if no resources are stored.
+    def empty?
+      raise NotImplementedError, 
+            "#{self.class} should implement `#empty?`, but does not."
+    end
+
+    ##
+    # Maps input URIs to the resource URI in the backend.
+    #
+    # @todo Is it necessary for the backend to know about this? Can we just 
+    #   expect the correct URI to be passed to {#retrieve}, with any mapping 
+    #   handled by that point?
+    # @todo Why are we mapping to `*/data/*.nt`? That should be the uri of an 
+    #   RDF document, rather than the actual resource. Is this mapping covering 
+    #   for an error elsewhere?
+    #
+    # @param uri [String, RDF::URI] a URI or URI-like string
+    # @return [String] the mapped string
+    def get_resource_uri(uri)
+      case uri
+      when /dbpedia.org\/resource\//
+        'http://dbpedia.org/data/' + uri.gsub(/.+dbpedia.org\/resource\//, '') + '.nt'
+      else 
+        uri.to_s
+      end
+    end
+
+    ##
+    # @abstract checks whether the resource is in the cache
+    #
+    # @param uri [String, RDF::URI] a URI or URI-like string
+    # @return [Boolean]
+    def has_resource?(uri)
+      raise NotImplementedError,
+            "#{self.class} should implement `#has_resource?`, but does not."
+    end
+
+    ##
+    # @abstract Retrieves RDF statements for a selected resource.
+    #
+    # @todo Should this simply implement the base verison against
+    #   `RDF::Repository`?
+    # @todo What are we supposed to return if the resource does not exist?
+    #
+    # @param uri [String, RDF::URI] a URI or URI-like string
+    # @return [RDF::Enumerable] the statements representing requested resource
+    def retrieve(uri)
+      raise NotImplementedError,
+            "#{self.class} should implement `#retrieve!`, but does not."
+    end
   end
 end
--- lib/linked_data_fragments/repository.rb
+++ lib/linked_data_fragments/repository.rb
@@ -1,30 +1,62 @@
 module LinkedDataFragments
-  class Repository < BackendBase
+  ##
+  # A basic `RDF::Repository` backend. This simply wraps RDF::Repository with
+  # the smaller `BackendBase` interface.
+  #
 
-    def initialize
-      @repo = RDF::Repository.new
+  # @example with a new in-memory repository
+  #   backend = LinkedDataFragments.new
+  #   backend.retrieve('http://example.com/moomin')
+  #
+  # @example with an existing repository instance
+  #   my_repository = RDF::Repository.new
+  #   backend = LinkedDataFragments.new(repository: my_repository)
+  #   backend.retrieve('http://example.com/moomin')
+  #
+  class Repository < BackendBase
+    ##
+    # @param repository [RDF::Repository] a repository instance
+    def initialize(repository: RDF::Repository.new)
+      @repo = repository
     end
 
+    ##
+    # @see BackendBase#add
+    def add(uri)
+      uri_to_load = get_resource_uri(uri)
+      @repo.load(uri_to_load)
+    end
+
+    ##
+    # Removes all resources from the backend.
+    #
+    # @return [void]
+    def delete_all!
+      @repo.clear
+    end
+
+    ##
+    # @see BackendBase#empty?
+    def empty?
+      @repo.empty?
+    end
+
+    ##
+    # @see BackendBase#has_resource?
+    def has_resource?(uri)
+      @repo.has_subject?(RDF::URI.new(uri))
+    end
+
+    ##
+    # @see BackendBase#retrieve
     def retrieve(uri)
-      if !@repo.has_subject?(RDF::URI.new(uri))
+      if !has_resource?(uri)
         uri_to_load = get_resource_uri(uri)
         @repo.load(uri_to_load)
       end
 
       resulting_graph = @repo.query(:subject => RDF::URI.new(uri))
       return resulting_graph
-    end
-
-    def get_resource_uri(uri)
-      if uri.match(/dbpedia.org\/resource\//)
-        uri = 'http://dbpedia.org/data/' + uri.gsub(/.+dbpedia.org\/resource\//, '') + '.nt'
-      end
-
-      return uri
-    end
-
-    def delete_all
-      @repo.clear
     end
   end
 end
