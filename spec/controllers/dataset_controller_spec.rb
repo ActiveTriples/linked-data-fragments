4cbef4cf93c50f3dc66235fac9f182435686fda0
Tom Johnson <tom@dp.la>
2017-02-10 19:19:14 -0800

    Style changes on `dataset_spec.rb`
    Just cleaning up style and reducing unnecessary code here. A few
    `.statements.to_a.count == 0` checks are reduced to `.empty?`;
    everything else is just cleaning up appearances.

--- spec/controllers/dataset_controller_spec.rb
+++ spec/controllers/dataset_controller_spec.rb
@@ -1,54 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe DatasetController do
-  describe "index" do
+describe DatasetController do
+  describe 'index' do
+    context 'JSON-LD' do
+      before { get :index, :format => :jsonld }
 
-    context "JSON-LD" do
-      before do
-        get :index, :format => :jsonld
+      it 'should return a graph' do
+        expect(JSON::LD::Reader.new(response.body)).not_to be_empty
       end
-      it "should return a graph" do
-        expect(JSON::LD::Reader.new(response.body).statements.to_a.length).not_to eq 0
-      end
-      it "should be JSON-LD" do
-        expect(response.content_type).to eq "application/ld+json"
+
+      it 'should be JSON-LD' do
+        expect(response.content_type).to eq 'application/ld+json'
       end
     end
 
-    context "n-triples" do
-      before do
-        get :index, :format => :nt
+    context 'n-triples' do
+      before { get :index, :format => :nt }
+
+      it 'should return a graph' do
+        expect(RDF::NTriples::Reader.new(response.body)).not_to be_empty
       end
-      it "should return a graph" do
-        expect(RDF::NTriples::Reader.new(response.body).statements.to_a.length).not_to eq 0
-      end
-      it "should be n-triples" do
-        expect(response.content_type).to eq "application/n-triples"
+
+      it 'should be n-triples' do
+        expect(response.content_type).to eq 'application/n-triples'
       end
     end
 
     context "ttl" do
-      before do
-        get :index, :format => :ttl
+      before { get :index, :format => :ttl }
+
+      it 'should return a graph' do
+        expect(RDF::Turtle::Reader.new(response.body)).not_to be_empty
       end
-      it "should return a graph" do
-        expect(RDF::Turtle::Reader.new(response.body).statements.to_a.length).not_to eq 0
-      end
-      it "should be n-triples" do
-        expect(response.content_type).to eq "text/turtle"
+
+      it 'should be n-triples' do
+        expect(response.content_type).to eq 'text/turtle'
       end
     end
 
-    context "invalid" do
-      it "should be of type 404 Routing Error" do
-        expect{get :index, :format => :invalid}.to raise_error(ActionController::RoutingError)
+    context 'invalid' do
+      it 'should be of type 404 Routing Error' do
+        expect { get :index, format: :invalid}
+          .to raise_error(ActionController::RoutingError)
       end
 
-      it "should output valid response formats" do
-        expect{get :index, :format => :invalid}.to raise_error(ActionController::RoutingError, /[ttl].+[application\/ld\+json]/)
+      it 'should output valid response formats' do
+        expect { get :index, format: :invalid}
+          .to raise_error(ActionController::RoutingError,
+                          /[ttl].+[application\/ld\+json]/)
       end
     end
-
   end
 
   describe 'show' do
