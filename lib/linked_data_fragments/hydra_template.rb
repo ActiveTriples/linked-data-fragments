6c50e05b7c3f7914112614eca176e4bd316177c2
Tom Johnson <tom@dp.la>
2017-02-12 15:28:38 -0800

    Add lookup_endpoint support for HydraTemplate
    This is a shameless green version of lookup endpoint support. This goes
    a long way toward a naive fix for #48.

--- lib/linked_data_fragments/hydra_template.rb
+++ lib/linked_data_fragments/hydra_template.rb
@@ -1,6 +1,9 @@
 module LinkedDataFragments
   class HydraTemplate
-    CONTROL_REGEX = /{\??(.*?)}/.freeze
+    CONTROL_REGEX  = /{\??(.*?)}/.freeze
+    PATH_REGEX     = /{([^\?]*?)}/.freeze
+    QUERY_REGEX    = /{\?(.*?)}/.freeze
+    TEMPLATE_REGEX = %r((http://[^{}]*\/)(#{CONTROL_REGEX}\/?)+).freeze
 
     ##
     # @!attribute [r] template
@@ -20,6 +23,26 @@ module LinkedDataFragments
         .flatten # Reduce multiple matches
         .flat_map{|x| x.split(",")} # Split on commas for matches
         .map(&:strip) # Strip whitespace
+    end
+
+    ##
+    # Gives a list of valid `void:uriLookupEndpoint`s derived from the template.
+    #
+    # @todo Don't hard code control mappings
+    #
+    # @return [Array<String>] a list of lookup endpoints based on the template
+    # @see https://www.w3.org/TR/void/#lookup
+    def lookup_endpoints
+      if controls.include?('subject')
+        case template
+        when QUERY_REGEX
+          [template.match(TEMPLATE_REGEX)[1] + '?subject=']
+        when PATH_REGEX
+          controls.first == 'subject' ? [template.match(TEMPLATE_REGEX)[1]] : []
+        end
+      else
+        []
+      end
     end
 
     ##
--- spec/linked_data_fragments/hydra_template_spec.rb
+++ spec/linked_data_fragments/hydra_template_spec.rb
@@ -6,14 +6,15 @@ describe LinkedDataFragments::HydraTemplate do
   
   describe '#controls' do
     it 'should return the mapped controls' do
-      expect(subject.controls).to eq ['subject']
+      expect(subject.controls).to contain_exactly('subject')
     end
 
     context 'when given multiple controls' do
       let(:template) { 'http://localhost:4000/{?subject,predicate, object}' }
 
       it 'should return all of them' do
-        expect(subject.controls).to eq ['subject', 'predicate', 'object']
+        expect(subject.controls)
+          .to contain_exactly('subject', 'predicate', 'object')
       end
     end
 
@@ -29,11 +30,53 @@ describe LinkedDataFragments::HydraTemplate do
       let(:template) { 'http://localhost:4000/{subject}/{predicate}' }
 
       it 'should return them' do
-        expect(subject.controls).to eq ['subject', 'predicate']
+        expect(subject.controls).to contain_exactly('subject', 'predicate')
       end
     end
   end
 
+  describe '#lookup_endpoints' do
+    context 'with subject as an available control' do
+      it 'gives the template uri' do
+        expect(subject.lookup_endpoints)
+          .to contain_exactly 'http://localhost:4000/?subject='
+      end
+
+      it 'varies to the URI geven' do
+        template = 'http://example.com/{?subject}'
+        subject  = described_class.new(template)
+
+        expect(subject.lookup_endpoints)
+          .to contain_exactly 'http://example.com/?subject='
+      end
+    end
+
+    context 'with slashes to separate controls' do
+      let(:template) { 'http://localhost:4000/blah/{subject}/{predicate}' }
+
+      it 'gives the template uri' do
+        expect(subject.lookup_endpoints)
+          .to contain_exactly 'http://localhost:4000/blah/'
+      end
+    end
+
+    context 'with non-appendable subject control' do
+      let(:template) { 'http://localhost:4000/blah/{predicate}/{subject}' }
+      
+      it 'is empty' do
+        expect(subject.lookup_endpoints).to be_empty
+      end
+    end
+
+    context 'with no subject controls' do
+      let(:template) { 'http://localhost:4000/{?predicate,object}' }
+      
+      it 'is empty' do
+        expect(subject.lookup_endpoints).to be_empty
+      end
+    end
+  end
+  
   describe '#to_s' do
     it 'should be the template' do
       expect(subject.to_s).to eql template
