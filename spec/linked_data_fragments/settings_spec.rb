e3c5954df05538e4a7c23c6674130d9430468ae7
Tom Johnson <tom@dp.la>
2017-02-13 10:02:36 -0800

    Add a test for default app root
    `Settings` is largely untested; This was an easy test to pick off, so
    I'm submitting it.

--- spec/linked_data_fragments/settings_spec.rb
+++ spec/linked_data_fragments/settings_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe LinkedDataFragments::Settings do
   describe '.app_root' do
-    it 'gives a relative path string "." by default'
+    it 'gives a relative path string "." by default' do
+      expect(described_class.app_root).to eq '.'
+    end
+    
     it 'when called within a rails app gives Rails.root'
     it 'gives APP_ROOT when available'
   end
@@ -16,9 +19,5 @@ describe LinkedDataFragments::Settings do
       expect(described_class.config_path)
         .to eq "#{described_class.app_root}/config/ldf.yml"
     end
-  end
-  
-  describe '.env' do
-    it 'defaults to "development"'
   end
 end
